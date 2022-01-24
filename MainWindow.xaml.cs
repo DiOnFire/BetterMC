431bf2e52dedc1abf3857315985acd476fcf13db
DiOnFire <di0nik.gd@gmail.com>
2022-01-24 12:42:48 +0300

    Added some logic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,5 +9,23 @@
         {
             InitializeComponent();
         }
+
+        /*
+         * Executes when we click close button
+         * Closes launcher window
+         */
+        private void OnCloseClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        /*
+         * Executes when we click minimize button
+         * Minimizes launcher window
+         */ 
+        private void OnMinimizeButtonClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Minimized;
+        }
     }
 }
