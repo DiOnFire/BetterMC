c7d7d4de24cd54e1f2ece655251d242575b30a9a
DiOnFire <di0nik.gd@gmail.com>
2022-01-25 10:15:21 +0300

    Куча всего крутого

---
+++ Core/ObserableObject.cs
@@ -0,0 +1,6 @@
+﻿namespace BetterMC.Core
+{
+    internal class ObserableObject
+    {
+    }
+}
---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BetterMC.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterMC.MVVM.ViewModel
+{
+    class MainViewModel : ObserableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
