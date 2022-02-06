970dea7bc348b542711350af80befaf09fb3c390
DiOnFire <di0nik.gd@gmail.com>
2022-02-06 22:44:22 +0300

    loaderrrrr

---
+++ Core/Installations/Loader/LaunchArguments.cs
@@ -0,0 +1,18 @@
+﻿namespace BetterMC.Core.Installations.Loader
+{
+    public sealed class LaunchArguments
+    {
+        public static string DefaultJavaArgs =
+            "-XX:+UnlockExperimentalVMOptions " +
+                "-XX:+UseG1GC " +
+                "-XX:G1NewSizePercent=20 " +
+                "-XX:G1ReservePercent=20 " +
+                "-XX:MaxGCPauseMillis=50 " +
+                "-XX:G1HeapRegionSize=16M";
+
+        public static void BuildArgs()
+        {
+
+        }
+    }
+}
