18da902e81261d975b88d80dd56463f71df1106c
DiOnFire <di0nik.gd@gmail.com>
2022-03-31 01:09:10 +0300

    account class refactor

--- Core/Accounts/Account.cs
+++ Core/Accounts/Account.cs
@@ -1,18 +1,78 @@
-﻿namespace BetterMC.Core.Accounts
-{
-    public struct Account
-    {
-        public string login { get; set; }
-        public string password { get; set; }
-        public string accessToken { get; set; }
-        public AccountType type { get; }
+﻿using BetterMC.Core.Exception;
 
-        public Account(string login, AccountType type, string password = "", string accessToken = "")
+namespace BetterMC.Core.Accounts
+{
+    public sealed class Account
+    {
+        private string login, password, accessToken;
+        private readonly AccountType type;
+
+        public string Login
         {
-            this.login = login;
-            this.password = password;
+            get { return login; }
+            set
+            {
+                if (login.Length == 0) throw new EmptyLoginException("Empty login.");
+                login = value;
+                // TODO: Validation
+            }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                if (type == AccountType.CRACKED || type == AccountType.MICROSOFT) throw new InvalidTypeException("Password cannot be set.");
+                if (value.Length == 0) throw new EmptyPasswordException("Empty password.");
+                password = value;
+                // TODO: Validation
+            }
+        }
+
+        public string AccessToken
+        {
+            get { return accessToken; }
+            set
+            {
+                if (type != AccountType.MICROSOFT) throw new InvalidTypeException("Access token cannot be set to non-microsoft account.");
+                if (value.Length == 0) throw new EmptyTokenException("Empty token");
+                accessToken = value;
+                // TODO: Validation
+            }
+        }
+
+        public AccountType Type
+        {
+            get { return type; }
+        }
+
+        public Account(string login, AccountType type)
+        {
+            switch (type)
+            {
+                case AccountType.CRACKED:
+                    this.Login = login;
+                    break;
+                case AccountType.MICROSOFT:
+                    this.AccessToken = login;
+                    break;
+                default:
+                    throw new InvalidTypeException("Unknown account type.");
+            }
             this.type = type;
-            this.accessToken = accessToken;
+        }
+
+        public Account(string login, string password)
+        {
+            this.Login = login;
+            this.Password = password;
+            this.type = AccountType.MOJANG;
+        }
+
+        public void Auth()
+        {
+
         }
     }
 }
---
+++ Core/Exception/Account/InvalidCredentialsException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BetterMC.Core.Exception
+{
+    abstract class InvalidCredentialsException : ArgumentException
+    {
+        public InvalidCredentialsException(string message) : base(message) { }
+    }
+
+    sealed class EmptyLoginException : InvalidCredentialsException
+    {
+        public EmptyLoginException(string message) : base(message) { }
+    }
+
+    sealed class InvalidLoginException : InvalidCredentialsException
+    {
+        public InvalidLoginException(string message) : base(message) { }
+    }
+
+    sealed class EmptyPasswordException : InvalidCredentialsException
+    {
+        public EmptyPasswordException(string message) : base(message) { }
+    }
+
+    sealed class InvalidPasswordException : InvalidCredentialsException
+    {
+        public InvalidPasswordException(string message) : base(message) { }
+    }
+
+    sealed class InvalidTypeException : InvalidCredentialsException
+    {
+        public InvalidTypeException(string message) : base(message) { }
+    }
+    
+    sealed class EmptyTokenException : InvalidCredentialsException
+    {
+        public EmptyTokenException(string message) : base(message) { }
+    }
+}
